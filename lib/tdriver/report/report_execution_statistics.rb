436b494066505b258f2936b673914162eb680542
Mika Leiman <ext-mika.1.leiman@nokia.com>
2010-08-13 13:43:24 +0300

    Fixed the links is statistics page that were still off if the test executions had different amount of executions.

--- lib/tdriver/report/report_execution_statistics.rb
+++ lib/tdriver/report/report_execution_statistics.rb
@@ -72,6 +72,7 @@ class ReportingStatistics
     @group_test_case_arr.each do |test_case|
       tc_status=test_case[7]
       tc_name=test_case[0].to_s.gsub('_',' ')
+      tc_execution=test_case[8].to_i
       reboots=test_case[2]
       crashes=test_case[3]
       current_index=0
@@ -95,19 +96,19 @@ class ReportingStatistics
       @statistics_arr.each do |total_status|
         if total_status[1]==tc_status && total_status[0]==tc_name
           b_test_in_statistics=true
-          @statistics_arr[current_index]=[tc_name,tc_status,total_status[2].to_i+1]
+          @statistics_arr[current_index]=[tc_name,tc_status,total_status[2].to_i+1,tc_execution]
         end
         if total_status[1]=="reboots" && total_status[0]==tc_name
           b_test_in_statistics=true
-          @statistics_arr[current_index]=[tc_name,"reboots",total_status[2].to_i+reboots.to_i]
+          @statistics_arr[current_index]=[tc_name,"reboots",total_status[2].to_i+reboots.to_i,tc_execution]
         end
         if total_status[1]=="crashes" && total_status[0]==tc_name
           b_test_in_statistics=true
-          @statistics_arr[current_index]=[tc_name,"crashes",total_status[2].to_i+crashes.to_i]
+          @statistics_arr[current_index]=[tc_name,"crashes",total_status[2].to_i+crashes.to_i,tc_execution]
         end
         if total_status[1]=="total" && total_status[0]==tc_name
           b_test_in_statistics=true
-          @statistics_arr[current_index]=[tc_name,"total",total_status[2].to_i+1]
+          @statistics_arr[current_index]=[tc_name,"total",total_status[2].to_i+1,tc_execution]
         end
         current_index+=1
       end
@@ -115,15 +116,15 @@ class ReportingStatistics
       if b_test_in_statistics==false
         @all_statuses.each do |status|
           if status==tc_status
-            @statistics_arr << [tc_name,tc_status,1]
+            @statistics_arr << [tc_name,tc_status,1,tc_execution]
           elsif status=="reboots"
-            @statistics_arr << [tc_name,"reboots",reboots.to_i]
+            @statistics_arr << [tc_name,"reboots",reboots.to_i,tc_execution]
           elsif status=="crashes"
-            @statistics_arr << [tc_name,"crashes",crashes.to_i]
+            @statistics_arr << [tc_name,"crashes",crashes.to_i,tc_execution]
           elsif status=="total"
-            @statistics_arr << [tc_name,"total",1]
+            @statistics_arr << [tc_name,"total",1,tc_execution]
           else
-            @statistics_arr << [tc_name,status,0]
+            @statistics_arr << [tc_name,status,0,tc_execution]
           end
         end
       end
@@ -143,6 +144,7 @@ class ReportingStatistics
 
     result_page=test_index/@test_results_per_page.to_i
     result_page_mod=test_index % @test_results_per_page.to_i
+
     if result_page_mod>0
       result_page=result_page.to_i+1
     end
@@ -170,7 +172,7 @@ class ReportingStatistics
       '</tr>'
 
     test_case_added=Array.new
-    test_index=1
+
     @statistics_arr.each do |test_case|
       tc_name=test_case[0].to_s.gsub('_',' ')
       if test_case_added.include?(tc_name)==false
@@ -178,12 +180,11 @@ class ReportingStatistics
         table_body << "<td>#{tc_name}</td>"
         @statistics_arr.each do |test_case_statistics|
           if test_case_statistics[0]==tc_name
-            table_body << "<td#{add_result_style_tag(test_case_statistics[1],test_case_statistics[2])}>#{add_result_link(test_case_statistics[0],test_case_statistics[1],test_case_statistics[2],test_index)}#{test_case_statistics[2]}</a></td>"
+            table_body << "<td#{add_result_style_tag(test_case_statistics[1],test_case_statistics[2])}>#{add_result_link(test_case_statistics[0],test_case_statistics[1],test_case_statistics[2],test_case_statistics[3])}#{test_case_statistics[2]}</a></td>"
           end
         end
         table_body << "</tr>"
         test_case_added << tc_name
-      test_index+=1
       end
     end
 
