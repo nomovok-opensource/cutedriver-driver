daad2ef09444a158919616c15f025b46c03d77ba
Mika Leiman <ext-mika.1.leiman@nokia.com>
2010-09-06 09:31:08 +0300

    Added logging for tracing the verify always fails and changed the settings reading so that each verify always can have it's own settings.

--- lib/tdriver/base/test_object/factory.rb
+++ lib/tdriver/base/test_object/factory.rb
@@ -29,6 +29,17 @@ module MobyBase
 
 		attr_reader :timeout
 
+    # TODO: Document me (TestObjectFactory::check_verify_always_reporting_settings)
+    def check_verify_always_reporting_settings()
+      @reporter_attached = MobyUtil::Parameter[ :report_attach_continuous_verification_to_reporter, 'false' ]
+
+      @rcv_raise_errors = MobyUtil::Parameter[ :report_continuous_verification_raise_errors, 'true' ]
+
+      @rcv_fail_test_case = MobyUtil::Parameter[ :report_continuous_verification_fail_test_case_on_error, 'true' ]
+
+      @rvc_capture_screen = MobyUtil::Parameter[ :report_continuous_verification_capture_screen_on_error, 'true' ]
+    end
+
 		# TODO: Document me (TestObjectFactory::initialize)
 		def initialize
 
@@ -39,15 +50,7 @@ module MobyBase
 
 			@test_object_cache = {}
 
-			@inside_verify = false
-
-      @reporter_attached = MobyUtil::Parameter[ :report_attach_continuous_verification_to_reporter, 'false' ]
-
-      @rcv_raise_errors = MobyUtil::Parameter[ :report_continuous_verification_raise_errors, 'true' ]
-
-      @rcv_fail_test_case = MobyUtil::Parameter[ :report_continuous_verification_fail_test_case_on_error, 'true' ]      
-
-      @rvc_capture_screen = MobyUtil::Parameter[ :report_continuous_verification_capture_screen_on_error, 'true' ]
+			@inside_verify = false      
 
 		end
 
@@ -162,7 +165,7 @@ module MobyBase
 				logging_enabled = MobyUtil::Logger.instance.enabled
 
 				sut.verify_blocks.each do | verify |
-
+          check_verify_always_reporting_settings
 					begin
 
 						MobyUtil::Logger.instance.enabled = false
@@ -187,6 +190,11 @@ module MobyBase
                 end
                 TDriverReportAPI::tdriver_report_log("Verification failed as an exception was thrown when the verification block was executed. %s\nDetails: %s\nNested exception:\n%s" % [ verify.source, ( verify.message || "none" ), e.inspect ])
                 TDriverReportAPI::tdriver_report_log("<hr />")
+                
+                MobyUtil::Logger.instance.enabled = logging_enabled
+
+						    MobyUtil::Logger.instance.log "behaviour" , "FAIL;Verification #{verify.message.nil? ? '' : '\"' << verify.message << '\" '}failed:#{e.to_s}.\n#{verify.timeout.nil? ? '' : ' using timeout ' + verify.timeout.to_s}.;#{sut.id.to_s+';sut'};{};verify_always;" << verify.expected.to_s
+
               end
 
 						end
@@ -205,8 +213,14 @@ module MobyBase
                 else
                   TDriverReportAPI::tdriver_capture_state(false)
                 end
-                TDriverReportAPI::tdriver_report_log("Verification failed. %s\nDetails: %s\nThe block did not return %s. It returned: %s" % [ verify.source, ( verify.message || "none" ), verify.expected.inspect, result.inspect ])
+                
+                TDriverReportAPI::tdriver_report_log("Verification failed. %s\nDetails: %s\nThe block did not return %s. It returned: %s " % [ verify.source, ( verify.message || "none" ), verify.expected.inspect, result.inspect])
                 TDriverReportAPI::tdriver_report_log("<hr />")
+
+                MobyUtil::Logger.instance.enabled = logging_enabled
+
+						    MobyUtil::Logger.instance.log "behaviour" , "FAIL;Verification #{verify.message.nil? ? '' : '\"' << verify.message << '\" '}failed:#{e.to_s}.\n#{verify.timeout.nil? ? '' : ' using timeout ' + verify.timeout.to_s}.;#{sut.id.to_s+';sut'};{};verify_always;" << verify.expected.to_s
+
               end
 
 						end
