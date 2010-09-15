53cefa6c8b80c8c8ddb64901b5d7f2379efee1b5
Olli Ruotsalainen <ext-olli.1.ruotsalainen@nokia.com>
2010-09-15 13:55:26 +0300

    Changed linux webcam streamer threading and stream termination.

--- lib/tdriver/util/video_rec.rb
+++ lib/tdriver/util/video_rec.rb
@@ -176,7 +176,7 @@ module MobyUtil
 		# RuntimeError: No filename has been defined or recording initialization failed due to timeout.
 		def start_recording
 
-          raise RuntimeError.new("No video file defined, unable to start recording.") unless !@_video_file.nil?
+          raise RuntimeError.new( "No video file defined, unable to start recording." ) unless !@_video_file.nil?
 
 		  if File.exists?( @_video_file )		    
 		    begin
@@ -185,8 +185,16 @@ module MobyUtil
 		      # no reaction to failed file ops, unless recording fails
 		    end
 		  end	
-                  
-          @_rec_pipe = IO.popen('streamer -q -c /dev/video0 -f rgb24 -r ' + @_rec_options[ :fps ].to_s + ' -t 99:00:00 -o ' +  @_video_file.to_s + ' -s ' + @_rec_options[ :width ].to_s + 'x' + @_rec_options[ :height ].to_s + ' 2>&1', 'w+')
+
+          rec_command = 'streamer -q -c /dev/video0 -f rgb24 -r ' + @_rec_options[ :fps ].to_s + ' -t 99:00:00 -o ' +  @_video_file.to_s + ' -s ' + @_rec_options[ :width ].to_s + 'x' + @_rec_options[ :height ].to_s
+          
+          @_streamer_pid = fork do
+            begin
+              Kernel::exec( rec_command )
+            rescue Exception => e
+              raise RuntimeError.new( "An error was encountered while launching streamer:\n" << e.inspect )
+            end
+          end
 
           file_timed_out = false
           file_timeout = Time.now + STARTUP_TIMEOUT
@@ -203,8 +211,7 @@ module MobyUtil
 		  if file_timed_out
 		    # make sure recording is not initializing, clean up any failed file		    
 			begin
-			  Process.kill(SIGKILL, @_rec_pipe.pid)  			
-              @_rec_pipe.close
+			  Process.kill( 9, @_streamer_pid.to_i )
 			rescue
 			end
 			
@@ -227,8 +234,7 @@ module MobyUtil
 		def stop_recording 
           if @_recording		  
             @_recording = false						
-            Process.kill(9, @_rec_pipe.pid)  		
-            @_rec_pipe.close	
+            Process.kill( 9, @_streamer_pid.to_i )
 		  end
 		  return nil
 		end		
