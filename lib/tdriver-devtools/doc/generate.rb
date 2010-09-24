49839aedeeb63dd7eab157bd809f6fcc03d39e72
Jussi Koljonen <ext-jussi.1.koljonen@nokia.com>
2010-09-24 15:21:09 +0300

    tdriver-devtools: doc generator update

--- lib/tdriver-devtools/doc/generate.rb
+++ lib/tdriver-devtools/doc/generate.rb
@@ -404,11 +404,475 @@ def collect_feature_tests
 
 end
 
+def generate_document_xml
+
+=begin
+
+ <feature>
+    <name>z</name>
+    <module>MobyBehaviour::QT::Gesture</module>
+    <full_name>MobyBehaviour::QT::Gesture#z</full_name>
+    <type>accessor</type>
+    <arguments>
+      <count>0</count>
+      <optional>0</optional>
+    </arguments>
+    <feature_documentation>
+      <describes>
+        <name>z</name>
+        <name>z=</name>
+      </describes>
+      <description>example desc</description>
+      <info></info>
+      <behaviour_name>QtExampleGestureBehaviour</behaviour_name>
+      <required_plugin>*</required_plugin>
+      <sut_types>
+        <type>qt</type>
+      </sut_types>
+      <sut_versions>
+        <version>*</version>
+      </sut_versions>
+      <object_types>
+        <version>*</version>
+        <version>sut</version>
+      </object_types>
+      <input_types>
+        <type>touch</type>
+      </input_types>
+      <arguments>
+        <argument>
+          <name>value</name>
+          <optional>false</optional>
+          <types>
+            <type>
+              <name>Integer</name>
+              <example>10</example>
+              <description>Example argument1</description>
+            </type>
+          </types>
+        </argument>
+      </arguments>
+      <returns>
+        <type>
+          <name>String</name>
+          <example>"World"</example>
+          <description>Return value type</description>
+        </type>
+      </returns>
+      <exceptions/>
+    </feature_documentation>
+    <feature_tests/>
+  </feature>
+
+ <feature>
+
+    <type>accessor</type>
+
+    <implements>
+      <name>z</name>
+      <name>z=</name>
+    </implements>
+
+    <behaviour_name>QtExampleGestureBehaviour</behaviour_name>
+    <module>MobyBehaviour::QT::Gesture</module>
+    <required_plugin>*</required_plugin>
+
+    <sut_types>
+      <type>qt</type>
+    </sut_types>
+
+    <sut_versions>
+      <version>*</version>
+    </sut_versions>
+
+    <object_types>
+      <version>*</version>
+      <version>sut</version>
+    </object_types>
+
+    <input_types>
+      <type>touch</type>
+    </input_types>
+
+    <description>example desc</description>
+    <info></info>
+
+    <arguments>
+
+      <count>1</count>
+      <optional>0</optional>
+
+        <argument>
+          <name>value</name>
+          <optional>false</optional>
+          <types>
+            <type>
+              <name>Integer</name>
+              <example>10</example>
+              <description>Example argument1</description>
+            </type>
+          </types>
+        </argument>
+
+    </arguments>
+
+    <returns>
+
+      <type>
+        <name>String</name>
+        <example>"World"</example>
+        <description>Return value type</description>
+      </type>
+
+    </returns>
+      
+    <exceptions/>
+
+    <feature_tests/>
+
+  </feature>
+
+
+
+
+ <feature>
+
+    <type>accessor</type>
+
+    <implements>
+      <name>z</name>
+      <name>z=</name>
+    </implements>
+
+    <behaviour_name>QtExampleGestureBehaviour</behaviour_name>
+    <module>MobyBehaviour::QT::Gesture</module>
+    <required_plugin>*</required_plugin>
+
+    <sut_types>
+      <type>qt</type>
+    </sut_types>
+
+    <sut_versions>
+      <version>*</version>
+    </sut_versions>
+
+    <object_types>
+      <version>*</version>
+      <version>sut</version>
+    </object_types>
+
+    <input_types>
+      <type>touch</type>
+    </input_types>
+
+    <description>example desc</description>
+    <info></info>
+
+    <arguments count="1" optional="0">
+
+      <argument name="value" optional="false">
+        <type name="Integer">
+          <example>10</example>
+          <description>Example argument1</description>
+        </type>
+      </argument>
+
+    </arguments>
+
+    <returns>
+
+      <type name="String">
+        <example>"World"</example>
+        <description>Return value type</description>
+      </type>
+
+    </returns>
+      
+    <exceptions/>
+
+    <feature_tests/>
+
+  </feature>
+
+
+
+
+=end
+
+
+  doc = Nokogiri::XML::Builder.new{ | xml |
+
+    #p doc.to_xml
+    #p doc.to_xml
+    #exit
+
+    xml.documentation{
+
+      # TODO: behaviour.hash should have feature type (method/attribute) mentioned  
+      # TODO: behaviour.hash should have number of arguments incl. optional + blocks
+
+      collect_all_features.sort.each{ | feature |
+      
+        module_name, method_name, feature_type, feature_parameters = feature.split("#")
+
+        feature_parameters = feature_parameters.split(";")
+        
+        feature = "%s#%s" % [ module_name, method_name ]
+      
+        # get document
+
+        documented = @documented_features.keys.include?( feature )
+
+        if documented 
+
+          feature_documentation = @documented_features[ feature.to_s ]
+                      
+          xml.describes{
+
+            feature_documentation["name"].each{ | feature_name |
+            
+            xml.name( feature_name )
+
+            }
+
+          }
+
+        else
+
+          
+
+        end
+        
+        xml.feature(){ 
+
+          xml.name!( method_name.to_s )
+          xml.module( module_name.to_s )
+          xml.full_name( feature.to_s ) 
+
+          xml.type!( feature_type )
+                          
+          xml.arguments{
+          
+            xml.count( feature_parameters[ 0 ] )
+            xml.optional( feature_parameters[ 1 ] )
+          
+          }
+                          
+          xml.feature_documentation!{ 
+                
+            if documented
+
+              feature_documentation = @documented_features[ feature.to_s ]
+                          
+              xml.describes{
+
+                feature_documentation["name"].each{ | feature_name |
+                
+                xml.name( feature_name )
+
+                }
+
+              }
+              
+              xml.description( feature_documentation[ "description" ] )
+
+              xml.info( feature_documentation[ "info" ] )
+
+              xml.behaviour_name( feature_documentation[ "__behaviour" ][ "name" ] )
+
+              xml.required_plugin( feature_documentation[ "__behaviour" ][ "plugin" ] )
+
+              xml.sut_types{
+              
+                feature_documentation["__behaviour"][ "sut_type" ].each{ | sut_type |
+                
+                  xml.type!( sut_type )
+                
+                }
+              }
+
+              xml.sut_versions{
+              
+                feature_documentation["__behaviour"][ "version" ].each{ | sut_version |
+                
+                  xml.version!( sut_version )
+                
+                }
+              }
+
+              xml.object_types{
+              
+                feature_documentation["__behaviour"][ "object_type" ].each{ | object_type |
+                
+                  xml.version!( object_type )
+                
+                }
+              }
+
+
+              xml.input_types{
+              
+                feature_documentation["__behaviour"][ "input_type" ].each{ | input_type |
+                
+                  xml.type!( input_type )
+                
+                }
+              }
+              
+              xml.arguments{ 
+              
+                ( feature_documentation[ "arguments" ] || [] ).each{ | argument |
+                
+                  xml.argument{
+                  
+                    xml.name!( ( argument[ "name" ] || ["NoMethodName"] ).first ) 
+                    
+                    xml.optional( ( argument[ "optional" ] || [] ).first == "true" )
+       
+                    xml.types{ 
+                    
+                      argument[ "types" ].each{ | type |
+                      
+                        type.each_pair{ | key, value |
+
+                            xml.type!{ 
+
+                              xml.name!( key.to_s )
+
+                              # store each key & value as is                        
+                              value.each_pair{ | key, value | 
+                                
+                                xml.send( key.to_sym, value.to_s )
+
+                              } # type.value.each_pair
+                            
+                            } # type
+                            
+                        } # type.each_pair
+                        
+                      } # types.each
+                    
+                    } # xml.types
+                    
+                  } # xml.argument
+                              
+                } # xml.arguments.each
+                            
+              } # xml.arguments
+              
+
+              xml.returns{
+              
+                ( feature_documentation[ "returns" ] || [] ).each{ | returns |
+
+                  returns.each_pair{ | key, value |
+
+                      xml.type!{ 
+
+                        xml.name!( key.to_s )
+
+                        # store each key & value as is                        
+                        value.each_pair{ | key, value | 
+                          
+                          xml.send( key.to_sym, value.to_s )
+
+                        } # type.value.each_pair
+                      
+                      } # type
+                      
+                  } # type.each_pair
+
+                } # returns.each
+              
+              } # xml.returns
+
+              xml.exceptions{
+              
+                ( feature_documentation[ "exceptions" ] || [] ).each{ | returns |
+
+                  returns.each_pair{ | key, value |
+
+                      xml.type!{ 
+
+                        xml.name!( key.to_s )
+
+                        # store each key & value as is                        
+                        value.each_pair{ | key, value | 
+                          
+                          xml.send( key.to_sym, value.to_s )
+
+                        } # type.value.each_pair
+                      
+                      } # type
+                      
+                  } # type.each_pair
+
+                } # returns.each
+              
+              } # xml.exceptions
+                  
+            end
+            
+          }
+                  
+          
+          xml.feature_tests!{
+      
+            case feature_type
+            
+              when /accessor/i
+                tests = [ feature, "%s=" % feature ]
+
+              when /writer/i
+                tests = [ "%s=" % feature ]
+              
+              when /method/i, /reader/i            
+                tests = [ feature ]
+            
+            end
+
+            #tests = ["MobyBehaviour::QT::Widget#tap"]
+            
+            tests.each{ | test_feature |
+                  
+              ( @executed_tests[ test_feature ] || [] ).each{ | test |
+              
+                xml.scenario{ 
+
+                  xml.type!( feature_type == "accessor" ? ( ( test_feature[-1] == ?= ) ? "writer" : "reader" ) : feature_type )
+                      
+                  xml.status( test[ "status" ] )
+                  xml.description( test[ "description" ] )
+                  xml.example( test[ "example" ] )
+                
+                } # xml.scenario
+                            
+              } # executed_tests.each
+                    
+            }        
+                      
+          } # xml.feature_tests
+          
+        }
+
+      }
+
+    }
+  }
+
+  doc.to_xml
+
+end
+
+
+
 if ARGV.count < 1
   
   abort "\nUsage: #{ File.basename( $0 ) } feature_xml_folder [behaviour_xml_folder]\n\n"
 
 end
+
+
 
 read_test_result_files # ok
 read_behaviour_xml_files # ok
@@ -428,243 +892,5 @@ puts "", "----", ""
 
 accessors = []
 
-doc = Nokogiri::XML::Builder.new{ | xml |
-
-  #p doc.to_xml
-  #p doc.to_xml
-  #exit
-
-  xml.documentation{
-
-    # TODO: behaviour.hash should have feature type (method/attribute) mentioned  
-    # TODO: behaviour.hash should have number of arguments incl. optional + blocks
-
-    collect_all_features.sort.each{ | feature |
-    
-      module_name, method_name, feature_type, feature_parameters = feature.split("#")
-
-      feature_parameters = feature_parameters.split(";")
-      
-      feature = "%s#%s" % [ module_name, method_name ]
-    
-      xml.feature{ 
-
-        documented = @documented_features.keys.include?( feature )
-
-        xml.name!( method_name.to_s )
-        xml.module( module_name.to_s )
-        xml.full_name( feature.to_s ) 
-
-        xml.type!( feature_type )
-                        
-        xml.arguments{
-        
-          xml.count( feature_parameters[0] )
-          xml.optional( feature_parameters[1] )
-        
-        }
-                        
-        xml.feature_documentation!{ 
-              
-          if documented
-
-            feature_documentation = @documented_features[ feature.to_s ]
-                        
-            xml.describes{
-
-              feature_documentation["name"].each{ | feature_name |
-              
-              xml.name( feature_name )
-
-              }
-
-            }
-            
-            xml.description( feature_documentation[ "description" ] )
-
-            xml.info( feature_documentation[ "info" ] )
-
-            xml.behaviour_name( feature_documentation[ "__behaviour" ][ "name" ] )
-
-            xml.required_plugin( feature_documentation[ "__behaviour" ][ "plugin" ] )
-
-            xml.sut_types{
-            
-              feature_documentation["__behaviour"][ "sut_type" ].each{ | sut_type |
-              
-                xml.type!( sut_type )
-              
-              }
-            }
-
-            xml.sut_versions{
-            
-              feature_documentation["__behaviour"][ "version" ].each{ | sut_version |
-              
-                xml.version!( sut_version )
-              
-              }
-            }
-
-            xml.object_types{
-            
-              feature_documentation["__behaviour"][ "object_type" ].each{ | object_type |
-              
-                xml.version!( object_type )
-              
-              }
-            }
-
-
-            xml.input_types{
-            
-              feature_documentation["__behaviour"][ "input_type" ].each{ | input_type |
-              
-                xml.type!( input_type )
-              
-              }
-            }
-            
-            xml.arguments{ 
-            
-              ( feature_documentation[ "arguments" ] || [] ).each{ | argument |
-              
-                xml.argument{
-                
-                  xml.name!( ( argument[ "name" ] || ["NoMethodName"] ).first ) 
-                  
-                  xml.optional( ( argument[ "optional" ] || [] ).first == "true" )
-     
-                  xml.types{ 
-                  
-                    argument[ "types" ].each{ | type |
-                    
-                      type.each_pair{ | key, value |
-
-                          xml.type!{ 
-
-                            xml.name!( key.to_s )
-
-                            # store each key & value as is                        
-                            value.each_pair{ | key, value | 
-                              
-                              xml.send( key.to_sym, value.to_s )
-
-                            } # type.value.each_pair
-                          
-                          } # type
-                          
-                      } # type.each_pair
-                      
-                    } # types.each
-                  
-                  } # xml.types
-                  
-                } # xml.argument
-                            
-              } # xml.arguments.each
-                          
-            } # xml.arguments
-            
-
-            xml.returns{
-            
-              ( feature_documentation[ "returns" ] || [] ).each{ | returns |
-
-                returns.each_pair{ | key, value |
-
-                    xml.type!{ 
-
-                      xml.name!( key.to_s )
-
-                      # store each key & value as is                        
-                      value.each_pair{ | key, value | 
-                        
-                        xml.send( key.to_sym, value.to_s )
-
-                      } # type.value.each_pair
-                    
-                    } # type
-                    
-                } # type.each_pair
-
-              } # returns.each
-            
-            } # xml.returns
-
-            xml.exceptions{
-            
-              ( feature_documentation[ "exceptions" ] || [] ).each{ | returns |
-
-                returns.each_pair{ | key, value |
-
-                    xml.type!{ 
-
-                      xml.name!( key.to_s )
-
-                      # store each key & value as is                        
-                      value.each_pair{ | key, value | 
-                        
-                        xml.send( key.to_sym, value.to_s )
-
-                      } # type.value.each_pair
-                    
-                    } # type
-                    
-                } # type.each_pair
-
-              } # returns.each
-            
-            } # xml.exceptions
-                
-          end
-          
-        }
-                
-        
-        xml.feature_tests!{
-    
-          case feature_type
-          
-            when /accessor/i
-              tests = [ feature, "%s=" % feature ]
-
-            when /writer/i
-              tests = [ "%s=" % feature ]
-            
-            when /method/i, /reader/i            
-              tests = [ feature ]
-          
-          end
-          
-          tests.each{ | test_feature |
-                
-            ( @executed_tests[ test_feature ] || [] ).each{ | test |
-            
-              xml.scenario{ 
-
-                xml.type!( feature_type == "accessor" ? ( ( test_feature[-1] == ?= ) ? "writer" : "reader" ) : feature_type )
-                    
-                xml.status( test[ "status" ] )
-                xml.description( test[ "description" ] )
-                xml.example( test[ "example" ] )
-              
-              } # xml.scenario
-                          
-            } # executed_tests.each
-                  
-          }        
-                    
-        } # xml.feature_tests
-        
-      }
-
-    }
-
-  }
-}
-
-puts doc.to_xml
-p accessors
-
+puts generate_document_xml
 
