ff8cac5075b7765087208d52f5dc7adf576cdd81
Mika Leiman <ext-mika.1.leiman@nokia.com>
2010-10-13 12:12:03 +0300

    Bug fix for file monitor where file retrieving with several file names was not working

--- lib/tdriver/report/report_file_capture.rb
+++ lib/tdriver/report/report_file_capture.rb
@@ -1,20 +1,20 @@
 ############################################################################
-## 
-## Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies). 
-## All rights reserved. 
-## Contact: Nokia Corporation (testabilitydriver@nokia.com) 
-## 
-## This file is part of Testability Driver. 
-## 
-## If you have questions regarding the use of this file, please contact 
-## Nokia at testabilitydriver@nokia.com . 
-## 
-## This library is free software; you can redistribute it and/or 
-## modify it under the terms of the GNU Lesser General Public 
-## License version 2.1 as published by the Free Software Foundation 
-## and appearing in the file LICENSE.LGPL included in the packaging 
-## of this file. 
-## 
+##
+## Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+## All rights reserved.
+## Contact: Nokia Corporation (testabilitydriver@nokia.com)
+##
+## This file is part of Testability Driver.
+##
+## If you have questions regarding the use of this file, please contact
+## Nokia at testabilitydriver@nokia.com .
+##
+## This library is free software; you can redistribute it and/or
+## modify it under the terms of the GNU Lesser General Public
+## License version 2.1 as published by the Free Software Foundation
+## and appearing in the file LICENSE.LGPL included in the packaging
+## of this file.
+##
 ############################################################################
 
 
@@ -45,8 +45,8 @@ module TDriverReportFileCapture
   end
 
   def clean_files_from_sut()
-   
-    if @monitor_files == 'true' && @clean_files == 'true'    
+
+    if @monitor_files == 'true' && @clean_files == 'true'
       MobyBase::SUTFactory.instance.connected_suts.each do |sut_id, sut_attributes|
         if sut_attributes[:is_connected]
           @file_suts.each do |monitored_sut|
@@ -68,36 +68,39 @@ module TDriverReportFileCapture
     @file_count=0
     #for every location:
     @file_locations.each do |location|
-          begin
-          #get file names
-          current_location_files = current_sut.list_files_from_sut( :from => location, :file=> @file_names.to_s )
+      begin
+        #get file names
+        current_location_files=Array.new
+        @file_names.each do |file_name|
+          current_location_files = current_sut.list_files_from_sut( :from => location, :file=> file_name.to_s )
           #collect crash names and add paths
           current_location_files.each do |sut_file|
             #if file_is_file(sut_file)
-              file_arr << sut_file.to_s
-              @file_count+=1
+            file_arr << sut_file.to_s
+            @file_count+=1
             #end
           end
-          rescue => ex
-             #puts ex.message
-          end
+        end
+      rescue => ex
+        #puts ex.message
+      end
     end
     file_arr
   end
 
   def download_file(current_sut,file_name,download_folder)
-    begin      
+    begin
       current_sut.copy_from_sut(:file => file_name, :to => download_folder )
     rescue => ex
-       #puts ex.message
+      #puts ex.message
     end
   end
 
   def delete_file(current_sut,file_name)
-    begin      
+    begin
       current_sut.delete_from_sut(:file => file_name )
     rescue => ex
-       #puts ex.message
+      #puts ex.message
     end
   end
 
@@ -120,7 +123,7 @@ module TDriverReportFileCapture
   end
 
   def download_files(download_folder)
-   if @monitor_files == 'true'
+    if @monitor_files == 'true'
       MobyBase::SUTFactory.instance.connected_suts.each do |sut_id, sut_attributes|
         if sut_attributes[:is_connected]
           @file_suts.each do |monitored_sut|
@@ -139,14 +142,14 @@ module TDriverReportFileCapture
 
   def capture_files()
     if @monitor_files == 'true'
-      begin       
+      begin
         dump_folder=@test_case_folder+'/files'
         if File::directory?(dump_folder)==false
           FileUtils.mkdir_p dump_folder
-        end        
+        end
         download_files(dump_folder)
       rescue Exception => e
-         @test_case_execution_log=@test_case_execution_log.to_s + '<br />' + "Unable to capture files: " + e.message + e.backtrace.to_s
+        @test_case_execution_log=@test_case_execution_log.to_s + '<br />' + "Unable to capture files: " + e.message + e.backtrace.to_s
       end
     end
   end
