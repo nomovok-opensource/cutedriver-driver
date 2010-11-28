2aca7c03885b79d8e199db734e6fd5346daa7457
Jussi Koljonen <ext-jussi.1.koljonen@nokia.com>
2010-11-29 01:20:18 +0200

    tdriver: factory_new.rb, adapter.rb; license header added

--- lib/tdriver/base/test_object/adapter.rb
+++ lib/tdriver/base/test_object/adapter.rb
@@ -1,3 +1,22 @@
+############################################################################
+## 
+## Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies). 
+## All rights reserved. 
+## Contact: Nokia Corporation (testabilitydriver@nokia.com) 
+## 
+## This file is part of Testability Driver. 
+## 
+## If you have questions regarding the use of this file, please contact 
+## Nokia at testabilitydriver@nokia.com . 
+## 
+## This library is free software; you can redistribute it and/or 
+## modify it under the terms of the GNU Lesser General Public 
+## License version 2.1 as published by the Free Software Foundation 
+## and appearing in the file LICENSE.LGPL included in the packaging 
+## of this file. 
+## 
+############################################################################
+
 module TDriver
 
   class TestObjectAdapter
@@ -77,7 +96,9 @@ module TDriver
 
     end
 
+    # enable hooking for performance measurement & debug logging
+    MobyUtil::Hooking.instance.hook_methods( self ) if defined?( MobyUtil::Hooking )
 
-  end
+  end # TestObjectAdapter
 
-end
+end # TDriver
