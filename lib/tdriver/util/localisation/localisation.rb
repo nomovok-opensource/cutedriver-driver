346695dbb5b03358f13c37f320ebf9ec5885d40c
Ivan Castane Capel <ivacasta@.NOE.Nokia.com>
2010-12-02 16:26:04 +0000

    Added common language map for symbian and maemo translations (.ts and .loc files)

--- lib/tdriver/util/localisation/localisation.rb
+++ lib/tdriver/util/localisation/localisation.rb
@@ -24,7 +24,222 @@ require 'nokogiri'
 module MobyUtil
 
 	class Localisation
-
+    
+    # == Language maping cross-referenced from Nokia Shared Lancuage Codes and Symbian 
+    # http://developer.symbian.org/main/documentation/reference/s3/pdk/GUID-31C133DE-F245-5992-9A41-20A99291E72A.html
+    # http://www2.connecting.nokia.com/nokia/terminology/terms.nsf/document/ES346PMF37?OpenDocument&ExpandSection=1#_Section1
+    # Its also enumerated by TLanguage in e32lang.h in the Symbian code.
+    # The ones only on e32lang.h are commented out
+    @language_code_map = {
+      "English" => ["en", "01"],
+      "French" => ["fr", "02"],
+      "German" => ["de", "03"],
+      "Spanish" => ["es", "04"],
+      "Italian" => ["it", "05"],
+      "Swedish" => ["sv", "06"],
+      "Danish" => ["da", "07"],
+      "Norwegian" => ["no", "08"], 
+      "Finish" => ["fi", "09"],
+      "English US" => ["en_US", "10"],
+      # "Swiss French" => ["SF", "11"],
+      # "Swiss German" => ["SG", "12"],
+      "Portuguese" => ["pt", "13"],
+      "Turkish" => ["tr", "14"],
+      "Icelandic" => ["is", "15"],
+      "Russian" => ["ru", "16"],  
+      "Hungarian" => ["hu", "17"],  
+      "Dutch" => ["nl", "18"],  
+      # "Belgian Flemish" => ["BL", "19"],  
+      # "Australian" => ["AU", "20"],  
+      # "Belgian French" => ["BF", "21"],  
+      # "Austrian" => ["AS", "22"],  
+      # "New Zealand" => ["NZ", "23"],  
+      # "International French" => ["IR", "24"],  
+      "Czech" => ["cs", "25"],  
+      "Slovak" => ["sk", "26"],  
+      "Polish" => ["pl", "27"],  
+      "Slovenian" => ["sl", "28"],  
+      "Chinese TW" => ["zh_TW", "29"],  
+      "Chinese HK" => ["zh_HK", "30"],  
+      "Chinese" => ["zh", "31"],  
+      "Japanese" => ["ja", "32"],  
+      "Thai" => ["th", "33"],  
+      "Afrikaans" => ["af", "34"],  
+      "Albanian" => ["sq", "35"],  
+      "Amharic" => ["am", "36"],  
+      "Arabic" => ["ar", "37"],  
+      "Armenian" => ["hy", "38"],  
+      "Filipino" => ["tl", "39"],
+      "Belarusian" => ["be", "40"],
+      "Bengali" => ["bn", "41"],
+      "Bulgarian" => ["bg", "42"],  
+      # "Burmese" => ["MY", "43"],  
+      "Catalan" => ["ca", "44"],  
+      "Croatian" => ["hr", "45"],  
+      # "Canadian English" => ["CE", "46"],  
+      # "International English" => ["IE", "47"],  
+      # "South African English" => ["SA", "48"],  
+      "Estonian" => ["et", "49"],  
+      "Persian" => ["fa", "50"],  
+      "French CA" => ["fr_CA", "51"],
+      # "Scots Gaelic" => ["GD", "52"],  
+      "Georgian" => ["ka", "53"],  
+      "Greek" => ["el", "54"],  
+      # "Cyprus Greek" => ["CG", "55"],  
+      "Gujarati" => ["gu", "56"],  
+      "Hebrew" => ["he", "57"],  
+      "Hindi" => ["hi", "58"],  
+      "Indonesian" => ["id", "59"],  
+      # "Irish" => ["GA", "60"],  
+      # "Swiss Italian" => ["SZ", "61"],  
+      "Kannada" => ["kn", "62"],  
+      "Kazakh" => ["kk", "63"],  
+      "Khmer" => ["km", "64"],  
+      "Korean" => ["ko", "65"],  
+      # "Lao" => ["lo", "66"],  
+      "Latvian" => ["lv", "67"],  
+      "Lithuanian" => ["lt", "68"],  
+      "Macedonian" => ["mk", "69"],  
+      "Malay" => ["ms", "70"],  
+      "Malayalam" => ["ml", "71"],  
+      "Marathi" => ["mr", "72"],  
+      # "Moldavian" => ["MO", "73"],  
+      "Mongolian" => ["mn", "74"],  
+      # "Norwegian Nynorsk" => ["nn", "75"],  
+      "Portuguese BR" => ["pt_BP", "76"],  
+      "Punjabi" => ["pa", "77"],  
+      "Romanian" => ["ro", "78"],  
+      "Servian" => ["sr", "79"], 
+      "Sinhala" => ["si", "80"],  
+      # "Somali" => ["SO", "81"],  
+      # "International Spanish" => ["OS", "82"],  
+      "Spanish AM" => ["es_419", "83"],  
+      "Swahili" => ["sw", "84"],  
+      # "Finland Swedish" => ["FS", "85"],  
+      "Tamil" => ["ta", "87"],   
+      "Telugu" => ["te", "88"],  
+      # "Tibetan" => ["BO", "89"],  
+      # "Tigrinya" => ["TI", "90"], 
+      # "Cyprus Turkish" => ["CT", "91"],  
+      "Turkem" => ["tk", "92"],  
+      "Ukranian" => ["uk", "93"],  
+      "Urdu" => ["ur", "94"],  
+      "Vietnamese" => ["vi", "96"],  
+      # "Welsh" => ["CY", "97"],
+      "Zulu" => ["zu", "98"],  
+      # "Manufacturer English" => ["ME", "100"],
+      "Sesotho" => ["st", "101"],
+      "Basque" => ["eu", "102"],
+      "Galician" => ["gl", "103"],
+      # "Javanese" => ["", "104"],
+      # "Maithili" => ["", "105"],
+      # "Azerbaijani Latin" => ["", "106"],
+      "Azerbaijani Cyrillic" => ["az", "107"],
+      "Oriya" => ["or", "108"],
+      # "Bhojpuri" => ["", "109"],
+      # "Sundanese" => ["", "110"],
+      # "Kurdish Latin" => ["", "111"],
+      # "Kurdish Arabic" => ["", "112"],
+      "Pashto" => ["ps", "113"],
+      "Hausa" => ["ha", "114"],
+      #"Oromo" => ["", "115"],
+      # "Uzbek Latin" => [", "116"],
+      "Uzbek Cyrillic" => ["uz", "117"],
+      # "Sindhi Arabic" => ["", "118"],
+      # "Sindhi Devanagari" => ["", "119"],
+      "Yoruba" => ["yo", "120"],
+      # "Cebuano" => ["", "121"],
+      "Igbo" => ["ig", "122"],
+      "Malagasy" => ["mg", "123"],
+      # "Nepali" => ["", "124"],
+      "Assamese" => ["as", "125"],
+      # "Shona" => ["", "126"],
+      # "Zhuang" => ["", "127"],
+      # "Madurese" => ["", "127"],
+      "English Apac" => ["EA", "129"],          # In e32long.h, Pearl script, but not in Nokia Language Codes Standard
+      "English Taiwan" => ["YW", "157"],        # In e32long.h, Pearl script, but not in Nokia Language Codes Standard
+      "English Hong Kong" => ["YH", "158"],     # In e32long.h, Pearl script, but not in Nokia Language Codes Standard
+      "English PRC" => ["YP", "159"],           # In e32long.h, Pearl script, but not in Nokia Language Codes Standard
+      "English Japan" => ["YJ", "160"],         # In e32long.h, Pearl script, but not in Nokia Language Codes Standard
+      "English Thailand" => ["YT", "161"],      # In e32long.h, Pearl script, but not in Nokia Language Codes Standard
+      # "Fulfulde" => ["", "162"], 
+      # "Tamazight" => ["", "163"], 
+      # "BolivianQuechua" => ["", "164"], 
+      # "PeruQuechua" => ["", "165"], 
+      # "EcuadorQuechua" => ["", "166"], 
+      "Tajik_Cyrillic" => ["tg", "167"], 
+      # "Tajik_PersoArabic" => ["", "168"], 
+      # "Nyanja" => ["", "169"], 
+      # "HaitianCreole" => ["", "170"], 
+      # "Lombard" => ["", "171"], 
+      # "Koongo" => ["", "172"],  
+      # "Akan" => ["", "173"], 
+      # "Hmong" => ["", "174"], 
+      # "Yi" => ["", "175"], 
+      # "Tshiluba" => ["", "176"], 
+      # "Ilocano" => ["", "177"], 
+      # "Uyghur" => ["", "178"], 
+      # "Neapolitan" => ["", "179"], 
+      # "Rwanda" => ["", "180"], 
+      "Xhosa" => ["xh", "181"], 
+      # "Balochi" => ["", "182"], 
+      # "Hiligaynon" => {"", "183"],
+      # "Minangkabau" => ["", "184"], 
+      # "Makhuwa" => ["", "185"], 
+      # "Santali" => ["", "186"], 
+      # "Gikuyu" => ["", "187"], 
+      # "Moore" => ["", "188"], 
+      # "Guarani" => ["", "189"],  
+      # "Rundi" => ["", "190"], 
+      # "Romani_Latin" => ["", "191"], 
+      # "Romani_Cyrillic" => ["", "192"], 
+      # "Tswana" => ["", "193"], 
+      # "Kanuri" => ["", "194"], 
+      # "Kashmiri Devanagari" => ["", "195"], 
+      "Kashmiri Perso Arabic" => ["ks", "196"], 
+      # "Umbundu" => ["", "197"], 
+      # "Konkani" => ["", "198"], 
+      # "Balinese" => ["", "199"], 
+      # "Northern Sotho" => ["", "200"], 
+      # "Wolof" => ["", "201"], 
+      # "Bemba" => ["", "202"], 
+      # "Tsonga" => ["", "203"], 
+      # "Yiddish" => ["", "204"], 
+      "Kirghiz" => ["ky", "205"], 
+      # "Ganda" => ["", "206"], 
+      # "Soga" => ["", "207"], 
+      # "Mbundu" => ["", "208"], 
+      # "Bambara" => ["", "209"], 
+      # "Central Aymara" => ["", "210"], 
+      # "Zarma" => ["", "211"], 
+      "Lingala" => ["ln", "212"], 
+      # "Bashkir" => ["", "213"], 
+      # "Chuvash" => ["", "214"], 
+      # "Swati" => ["", "215"], 
+      # "Tatar" => ["", "216"], 
+      # "Southern Ndebele" => ["", "217"], 
+      # "Sardinian" => ["", "218"], 
+      # "Scots" => ["", "219"], 
+      # "Meitei" => ["", "220"], 
+      # "Walloon" => ["", "221"], 
+      # "Kabardian" => ["", "222"], 
+      # "Mazanderani" => ["", "223"], 
+      # "Gilaki" => ["", "224"], 
+      # "Shan" => ["", "225"], 
+      # "Luyia" => ["", "226"], 
+      # "Luo" => ["", "227"], 
+      # "Sukuma" => ["", "228"], 
+      # "Aceh" => ["", "229"], 
+      #"English_India" => ["", "230"],           # In e32long.h, Pearl script, but not in Nokia Language Codes Standard
+      "Malay Apac" => ["MA", "326"],            # In e32long.h, Pearl script, but not in Nokia Language Codes Standard
+      # "Indonesian Apac" =>["", "327"],
+      # "Bengali IN" => ["bn_IN", ""],
+      # "Bosnian" => ["bs", ""],
+    }
+    
+    
+    
+    
 		# == description
 		# Function to fetch a translation for a given logical name from the localisation DB
 		#
@@ -337,31 +552,20 @@ module MobyUtil
 			return language, data
 		end
 		
+    #
+    # Note: for .loc files the colum mapping is done with the Language code number on the filenames
+    #
     def self.parse_loc_file(file, column_names_map = {})
     begin
-      # This language code table has been taken from 
-      # http://developer.symbian.org/main/documentation/reference/s3/pdk/GUID-31C133DE-F245-5992-9A41-20A99291E72A.html
-      # Its also enumerated by TLanguage in e32lang.h in the Symbian code.
-      language_code_map = {
-        "01" => "EN", "02" => "FR", "03" => "GE", "04" => "SP", "05" => "IT", "06" => "SW", "07" => "DA", "08" => "NO", "09" => "FI", 
-        "10" => "AM", "11" => "SF", "12" => "SG", "13" => "PO", "14" => "TU", "15" => "IC", "16" => "RU", "17" => "HU", "18" => "DU", "19" => "BL", 
-        "20" => "AU", "21" => "BF", "22" => "AS", "23" => "NZ", "24" => "IR", "25" => "CS", "26" => "SK", "27" => "PL", "28" => "SL", "29" => "TC", 
-        "30" => "HK", "31" => "ZH", "32" => "JA", "33" => "TH", "34" => "AF", "35" => "SQ", "36" => "AH", "37" => "AR", "38" => "HY", "39" => "TL", 
-        "40" => "BE", "41" => "BN", "42" => "BG", "43" => "MY", "44" => "CA", "45" => "HR", "46" => "CE", "47" => "IE", "48" => "SA", "49" => "ET", 
-        "50" => "FA", "51" => "CF", "52" => "GD", "53" => "KA", "54" => "EL", "55" => "CG", "56" => "GU", "57" => "HE", "58" => "HI", "59" => "IN", 
-        "60" => "GA", "61" => "SZ", "62" => "KN", "63" => "KK", "64" => "KM", "65" => "KO", "66" => "LO", "67" => "LV", "68" => "LT", "69" => "MK", 
-        "70" => "MS", "71" => "ML", "72" => "MR", "73" => "MO", "74" => "MN", "75" => "NN", "76" => "BP", "77" => "PA", "78" => "RO",  "79" => "SR",
-        "80" => "SI", "81" => "SO", "82" => "OS", "83" => "LS", "84" => "SH", "85" => "FS", "87" => "TA",   "88" => "TE", "89" => "BO", "90" => "TI",
-        "91" => "CT", "92" => "TK", "93" => "UK", "94" => "UR", "96" => "VI", "97" => "CY", "98" => "ZU", "100" => "ME", "101" => "ST", "129" => "EA",
-        "157" => "YW", "158" => "YH", "159" => "YP", "160" => "YJ", "161" => "YT", "326" => "MA"
-      }
-      
+    
       data = []
       file.split('/').last.match(/(.*)_(\w{2,3}).loc/)
       fname = $1
-      language_code = $2
-      language = language_code.to_s #language = language_code_map[ language_code ]
-			language = column_names_map[ language_code ] if !column_names_map.empty? and column_names_map.key?( language_code )
+      language_number = $2
+      # select returns an array of [language, codes] that suite the conditional
+      # codes is the array ["NokiaCode(2-leter)", "SymbianCode(number)"]
+      language = @language_code_map.select{|lang,codes| codes[1] == language_number}[0][1][0]   
+			language = column_names_map[ language_number ] if !column_names_map.empty? and column_names_map.key?( language_number )
       
       io = open(file)
       while line = io.gets
@@ -437,7 +641,7 @@ module MobyUtil
 			case db_connection.db_type
 				when "mysql"
 					begin
-						query_string = "ALTER TABLE `" + table_name + "` ADD  `" + language + "` VARCHAR(350) NULL DEFAULT NULL COLLATE utf8_general_ci;"
+						query_string = "ALTER TABLE `" + table_name + "` ADD  `" + language + "` TEXT NULL DEFAULT NULL COLLATE utf8_general_ci;"
 						MobyUtil::DBAccess.query( db_connection, query_string )
 						sql_file.write( query_string + "\n" ) if record_sql
 					rescue Mysql::Error # catch if language column already exists
@@ -512,7 +716,7 @@ module MobyUtil
 		
 		# == description
 		# Extracs application name used for FNAME from a given filename (removes language tags and file extension)
-		#
+		# Used only on .ts files
 		def self.parseFName(file)
 			fname = file.split('/').last
 			#(wordlist matching)
