d697b182e4e3b787b00782602ac07f4be99201ac
Jussi Koljonen <ext-jussi.koljonen@nokia.com>
2011-01-19 15:50:11 +0200

    tdriver#util/video: splitted classes/modules from video_util.rb into separate files

---
+++ lib/tdriver/util/video/loader.rb
@@ -0,0 +1,32 @@
+############################################################################
+## 
+## Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies). 
+## All rights reserved. 
+## Contact: Nokia Corporation (testabilitydriver@nokia.com) 
+## 
+## This file is part of Testability Driver. 
+## 
+## If you have questions regarding the use of this file, please contact 
+## Nokia at testabilitydriver@nokia.com . 
+## 
+## This library is free software; you can redistribute it and/or 
+## modify it under the terms of the GNU Lesser General Public 
+## License version 2.1 as published by the Free Software Foundation 
+## and appearing in the file LICENSE.LGPL included in the packaging 
+## of this file. 
+## 
+############################################################################
+
+
+# Video utils module
+require File.expand_path( File.join( File.dirname( __FILE__ ), 'video_utils.rb' ) )
+
+# camera windows implementation
+MobyUtil.autoload( :TDriverCam, File.expand_path( File.join( File.dirname( __FILE__ ), 'camera.rb' ) ) )
+
+# camera windows implementation
+MobyUtil.autoload( :TDriverWinCam, File.expand_path( File.join( File.dirname( __FILE__ ), 'camera_windows.rb' ) ) )
+
+# camera linux implementation
+MobyUtil.autoload( :TDriverLinuxCam, File.expand_path( File.join( File.dirname( __FILE__ ), 'camera_linux.rb' ) ) )
+
