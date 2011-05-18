b3d79fcb4e9923b1680eec03fc0d9aa45876add8
Jussi Koljonen <ext-jussi.koljonen@nokia.com>
2011-05-18 19:08:12 +0300

    tdriver#util/common/boolean.rb: Added true? and false? methods to TrueClass and FalseClass

---
+++ lib/tdriver/util/common/boolean.rb
@@ -0,0 +1,51 @@
+############################################################################
+## 
+## Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies). 
+## All rights reserved. 
+## Contact: Nokia Corporation (testabilitydriver@nokia.com) 
+## 
+## This file is part of Testability Driver. 
+## 
+## If you have questions regarding the use of this file, please contact 
+## Nokia at testabilitydriver@nokia.com . 
+## 
+## This library is free software; you can redistribute it and/or 
+## modify it under the terms of the GNU Lesser General Public 
+## License version 2.1 as published by the Free Software Foundation 
+## and appearing in the file LICENSE.LGPL included in the packaging 
+## of this file. 
+## 
+############################################################################
+
+# extend Ruby TrueClass and FalseClass class functionality
+class TrueClass
+
+  def true?
+
+    /^true$/i.match( self.to_s ) != nil
+  
+  end
+  
+  def false?
+  
+    false
+  
+  end
+
+end
+
+class FalseClass
+
+  def true?
+  
+    false
+  
+  end
+  
+  def false?
+  
+    /^false$/i.match( self.to_s ) != nil
+  
+  end
+
+end
